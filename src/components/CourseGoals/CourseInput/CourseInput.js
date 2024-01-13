fc90304e33fccb5956be8e5d73d2279b30cb07cc
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-01-13 12:40:21 +0500

    validation n dynamic styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,28 +6,40 @@ import "./CourseInput.css";
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
+  const [isGoalFocus, setIsGoalFocus] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    // if (event.target.value.trim().length > 0) {
-    //   setIsValid(true);
-    // }
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
+  };
+
+  const focusGoalHandler = () => {
+    setIsGoalFocus(false);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    // if (enteredValue.trim().length === 0) {
-    //   setIsValid(false);
-    //   return;
-    // }
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid && "invalid"}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          onBlur={focusGoalHandler}
+        />
+        {!isValid && !isGoalFocus && (
+          <p className="error_msg">Enter Something</p>
+        )}
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
